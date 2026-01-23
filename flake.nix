d14c04ea54acaa96ec2b35a48b3d6c4d74286478
ArikRahman <arik@tamu.edu>
2026-01-22 22:46:41 -0600

    Init, working bitwig UI gen

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Bitwig 6 Beta FHS Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.buildFHSEnv {
+        name = "bitwig-studio";
+
+        targetPkgs = pkgs: with pkgs; [
+          # Context for the fix:
+          cairo
+          xorg.xcbutilcursor # Replaces invalid 'cursor'
+          xorg.xcbutilwm     # Adds missing icccm lib
+          dbus
+
+          # Core dependencies
+          alsa-lib
+          # cursor
+          fontconfig
+          freetype
+          gdk-pixbuf
+          glib
+          gtk3
+          libglvnd
+          libxkbcommon
+          pipewire
+          vulkan-loader
+          zlib
+
+          # X11 libs
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXext
+          xorg.libXrandr
+          xorg.libxcb
+          xorg.xcbutil
+          xorg.xcbutilimage
+          xorg.xcbutilrenderutil
+        ];
+
+        runScript = "bash -c 'exec ./opt/bitwig-studio/bitwig-studio \"$@\"'";
+      };
+    };
+}
+
+
+
+#run with
+# nix run . -- -c "./opt/bitwig-studio/bitwig-studio"
